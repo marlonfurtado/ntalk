0c52664b9bb22acf13304cdd24162c4a3f24de90
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-01-15 13:30:28 -0200

    Arrumar layout

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cookieSession({name: 'session', keys: ['key1', 'key2']}));
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
 app.use(methodOverride('_method'));
 app.use(cookie);
 
